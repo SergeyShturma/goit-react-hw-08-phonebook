259bbc6ca3dab87190fdcba3cdec710ed814a49f
SergeyShturma <sergeyshturma@gmail.com>
2023-04-17 20:37:10 +0300

    delete double

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,18 +1,13 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getFilteredContacts, getIsLoading } from 'redux/selectors';
-import { deleteContact, getContacts } from 'redux/operations';
+import { deleteContact } from 'redux/operations';
 import s from './ContactList.module.css';
-import { useEffect } from 'react';
 import Loader from 'components/Loader';
 
 export const ContactList = () => {
   const dispatch = useDispatch();
   const isLoading = useSelector(getIsLoading);
   const contacts = useSelector(getFilteredContacts);
-
-  useEffect(() => {
-    dispatch(getContacts());
-  }, [dispatch]);
 
   const handleDelete = id => {
     dispatch(deleteContact(id));
